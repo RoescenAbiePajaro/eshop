57e6effcc62ba82a323752f8100311691a9f9999
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2024-07-22 09:52:26 -0500

    g

--- eshopp/src/app/app-routing.module.ts
+++ eshopp/src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FirstpageComponent } from './firstpage/firstpage.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+
+{path:'login',component:LoginComponent},
+{path:'signup',component:SignupComponent},
+{path:'dashboard',component:DashboardComponent},
+{path:'firstpage',component:FirstpageComponent},
+
+{
+  path: '', redirectTo: 'firstpage', pathMatch: 'full'
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- eshopp/src/app/app.module.ts
+++ eshopp/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
+import { FirstpageComponent } from './firstpage/firstpage.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    FirstpageComponent,
+    
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
