271791d8d425e93aa23a60c37f2024291f83f09f
RoescenAbiePajaro <157484846+RoescenAbiePajaro@users.noreply.github.com>
2024-07-24 11:57:30 -0500

    eyoo

--- eshopp/src/app/signup/signup.component.ts
+++ eshopp/src/app/signup/signup.component.ts
@@ -28,7 +28,7 @@ export class SignupComponent implements OnInit {
 
   signUpForm = new FormGroup({
     name: new FormControl('', Validators.required),
-    email: new FormControl('', [Validators.email, Validators.required]),
+    email: new FormControl('', [Validators.email, Validators.required,Validators.minLength(6)]),
     password: new FormControl('', Validators.required),
     confirmPassword: new FormControl('', Validators.required)
   }, { validators: passwordsMatchValidator() });
